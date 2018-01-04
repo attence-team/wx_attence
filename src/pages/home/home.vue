18b5957181a718c9b6da81045a3a5bb79424045b
yyg <yygwork@126.com>
2018-01-04 12:47:11 +0800

    修改首页导航

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -2,46 +2,48 @@
     <div class="body-box">
        <div class="carousel-box">
           <mt-swipe :show-indicators="false">
-             <mt-swipe-item v-for="simg in urls"><div class="img-item" :style="{backgroundImage: 'url('+simg.imgUrl+')'}" ></div></mt-swipe-item>
+             <mt-swipe-item v-for="simg in urls">
+             <div class="img-item" :style="{backgroundImage: 'url('+simg.imgUrl+')'}"></div>
+             </mt-swipe-item>
           </mt-swipe>
+          <div class="welcome">江楠，欢迎您！</div>
        </div>
-       <div class="welcome">江楠，欢迎您！</div>
        <div class="menu-nav">
           <div class="title"><i></i>我的工作</div>
           <div class="menu-list clearfix">
              <div class="item">
                 <router-link to="/work/pending">
-                  <img src="../../assets/img/3.png"><span>待办工作</span>
+                  <img src="../../assets/img/1_icon.png"><span>待办工作</span>
                 </router-link>
              </div>
              <div class="item">
                 <router-link to="/work/done">
-                  <img src="../../assets/img/3.png"><span>已办工作</span>
+                  <img src="../../assets/img/2_icon.png"><span>已办工作</span>
                 </router-link>
              </div>
           </div>
        </div>
        <div class="menu-nav">
-          <div class="title"><i></i>考勤管理 <router-link class="more" to="/home/kq">更多>></router-link></div>
+          <div class="title"><i></i>考勤管理 <router-link class="more" to="/home/kq">更多</router-link></div>
           <div class="menu-list clearfix">
              <div class="item">
                 <router-link to="/kq/leave">
-                  <img src="../../assets/img/3.png"><span>请假申请</span>
+                  <img src="../../assets/img/3_icon.png"><span>请假申请</span>
                 </router-link>
              </div>
              <div class="item">
                 <router-link to="/kq/yearLeave">
-                  <img src="../../assets/img/3.png"><span>年假申请</span>
+                  <img src="../../assets/img/4_icon.png"><span>年假申请</span>
                 </router-link>
              </div>
              <div class="item">
                 <router-link to="/kq/exception">
-                  <img src="../../assets/img/3.png"><span>异常处理</span>
+                  <img src="../../assets/img/5_icon.png"><span>异常处理</span>
                 </router-link>
              </div>
              <div class="item">
                 <router-link to="/kq/calendar">
-                  <img src="../../assets/img/3.png"><span>考勤日历</span>
+                  <img src="../../assets/img/6_icon.png"><span>考勤日历</span>
                 </router-link>
              </div>
           </div>
@@ -51,22 +53,22 @@
           <div class="menu-list clearfix">
              <div class="item">
                 <router-link to="/officework/sendCarApply">
-                  <img src="../../assets/img/3.png"><span>派车申请</span>
+                  <img src="../../assets/img/7_icon.png"><span>派车申请</span>
                 </router-link>
              </div>
              <div class="item">
                 <router-link to="/officework/workLunchApply">
-                  <img src="../../assets/img/3.png"><span>工作餐申请</span>
+                  <img src="../../assets/img/8_icon.png"><span>工作餐申请</span>
                 </router-link>
              </div>
              <div class="item">
                 <router-link to="/officework/sendCarQuery">
-                  <img src="../../assets/img/3.png"><span>派车单据查询</span>
+                  <img src="../../assets/img/9_icon.png"><span>派车单据查询</span>
                 </router-link>
              </div>
               <div class="item">
                 <router-link to="/officework/workLunchQuery">
-                  <img src="../../assets/img/3.png"><span>工作餐单据查询</span>
+                  <img src="../../assets/img/10_icon.png"><span>工作餐单据查询</span>
                 </router-link>
              </div>
           </div>
@@ -97,9 +99,9 @@ export default {
         return {
            msg:'',
            urls:[
-               {title:'1',imgUrl:'http://www.jq22.com/demo/myscroll201712192341/images/a2.png'},
-               {title:'2',imgUrl:'http://www.jq22.com/demo/myscroll201712192341/images/a3.png'},
-               {title:'3',imgUrl:'http://www.jq22.com/demo/myscroll201712192341/images/a5.png'}
+               {title:'1',imgUrl:'http://www.jq22.com/demo/jquerylbthk201712272345/img/3.jpg'},
+               {title:'2',imgUrl:'http://www.jq22.com/demo/jquerylbthk201712272345/img/5.jpg'},
+               {title:'3',imgUrl:'http://www.jq22.com/demo/jquerylbthk201712272345/img/3.jpg'}
            ],
            menuList:[],
            list:[]
@@ -125,29 +127,5 @@ export default {
 </script>
 
 <style lang="css" scoped>
-   .scroll-box{
-       height: calc(100vh - 120px);
-       overflow:hidden;
-       overflow-y:auto;
-       -webkit-overflow-scrolling: touch;
-   }
-   .page1{
-       padding-left: 0.1rem;
-   }
-   .box{
-       padding: 10px;
-   }
-   .img-item{
-       display: block;
-       width: 100%;
-       height: 100%;
-       background-size: cover;
-       background-position: center center;
-   }
-   .welcome{
-       text-align: right;
-       border-right: solid 0.2rem transparent;
-       padding: 0.15rem 0;
-       background-color: #F0F0F0;
-   }
+
 </style>
--- src/pages/kq/kq.vue
+++ src/pages/kq/kq.vue
@@ -1,59 +1,61 @@
 <template lang="html">
     <div class="body-box">
-       <mt-button type="primary" icon="back" @click="goBack()">返回</mt-button>
-       <div class="welcome">欢迎您：江楠</div>
+       <div class="carousel-box">
+          <mt-swipe :show-indicators="false">
+             <mt-swipe-item v-for="simg in urls">
+             <div class="img-item" :style="{backgroundImage: 'url('+simg.imgUrl+')'}"></div>
+             </mt-swipe-item>
+          </mt-swipe>
+          <div class="welcome">江楠，欢迎您！</div>
+       </div>
        <div class="menu-nav">
-          <div class="title">人事统计</div>
+          <div class="title"><i></i>人事统计</div>
           <div class="menu-list clearfix">
              <div class="item">
                 <router-link to="/kq/approval">
                   <mt-badge size="small">3</mt-badge>
-                  <img src="../../assets/img/3.png"><span>审批</span>
+                  <img src="../../assets/img/11_icon.png"><span>审批</span>
                 </router-link>
              </div>
              <div class="item">
                 <router-link to="/kq/exception">
-                  <img src="../../assets/img/3.png"><span>异常处理</span>
+                  <img src="../../assets/img/5_icon.png"><span>异常处理</span>
                 </router-link>
              </div>
           </div>
        </div>
        <div class="menu-nav">
-          <div class="title">发起申请</div>
+          <div class="title"><i></i>发起申请</div>
           <div class="menu-list clearfix">
              <div class="item">
                 <router-link to="/kq/leave">
-                  <img src="../../assets/img/3.png"><span>请假</span>
+                <mt-badge size="small">3</mt-badge>
+                  <img src="../../assets/img/3_icon.png"><span>请假</span>
                 </router-link>
              </div>
              <div class="item">
                 <router-link to="/kq/yearLeave">
-                  <img src="../../assets/img/3.png"><span>年休假</span>
+                  <img src="../../assets/img/4_icon.png"><span>年休假</span>
                 </router-link>
              </div>
              <div class="item">
                 <router-link to="/kq/leaveQuery">
-                  <img src="../../assets/img/3.png"><span>申请记录</span>
+                  <img src="../../assets/img/12_icon.png"><span>申请记录</span>
                 </router-link>
              </div>
           </div>
        </div>
        <div class="menu-nav">
-          <div class="title">相关查询</div>
+          <div class="title"><i></i>相关查询</div>
           <div class="menu-list clearfix">
              <div class="item">
                 <router-link to="/kq/cardRecord">
-                  <img src="../../assets/img/3.png"><span>原始刷卡记录</span>
+                  <img src="../../assets/img/13_icon.png"><span>原始刷卡记录</span>
                 </router-link>
              </div>
              <div class="item">
                 <router-link to="/kq/calendar">
-                  <img src="../../assets/img/3.png"><span>考勤日历</span>
-                </router-link>
-             </div>
-             <div class="item">
-                <router-link to="/kq/bill">
-                  <img src="../../assets/img/3.png"><span>考勤单据</span>
+                  <img src="../../assets/img/6_icon.png"><span>考勤日历</span>
                 </router-link>
              </div>
           </div>
@@ -61,22 +63,29 @@
     </div>
 </template>
 <script>
+import { Swipe, SwipeItem } from 'mint-ui';
 import { Badge } from 'mint-ui';
 export default {
     name:'kq',
-    components:{Badge},
+    components:{Badge,Swipe,SwipeItem},
     data(){
-        return {}
+        return {
+            urls:[
+                {title:'1',imgUrl:'http://www.jq22.com/demo/jQuerySlider201712071158/img/a5.png'},
+                {title:'2',imgUrl:'http://www.jq22.com/demo/jQuerySlider201712071158/img/a4.png'},
+                {title:'3',imgUrl:'http://www.jq22.com/demo/jQuerySlider201712071158/img/a2.png'}
+            ]
+        }
     },
     methods:{}
 }
 </script>
-
 <style lang="css" scoped>
-   .welcome{
-       text-align: right;
-       border-right: solid 0.2rem transparent;
-       padding: 0.15rem 0;
-       background-color: #F0F0F0;
-   }
+    .img-item{
+        display: block;
+        width: 100%;
+        height: 100%;
+        background-size: cover;
+        background-position: center center;
+    }
 </style>
