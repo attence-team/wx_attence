49c4c06b7006cc49e51f91529f9343771f542614
yyg <yygwork@126.com>
2018-01-08 09:59:42 +0800

    二级首页

--- src/pages/kq/kq.vue
+++ src/pages/kq/kq.vue
@@ -11,57 +11,41 @@
           </mt-swipe>
           <div class="welcome">江楠，欢迎您！</div>
        </div>
-       <div class="menu-box">
+       <div class="menu-box menu-tree">
            <div class="menu-nav">
-              <div class="title"><i class="person-icon"></i>人事统计</div>
+              <div class="title bd-bottom-1"><i class="person-icon"></i>人事统计</div>
               <div class="menu-list clearfix">
-                 <div class="item">
-                    <router-link to="/kq/approval">
-                      <mt-badge size="small">3</mt-badge>
-                      <img src="../../assets/img/11_icon.png"><span>审批</span>
-                    </router-link>
-                 </div>
-                 <div class="item">
-                    <router-link to="/kq/exception">
-                      <img src="../../assets/img/5_icon.png"><span>异常处理</span>
-                    </router-link>
-                 </div>
+                 <mt-cell title="审批" @click.native="goRouter('/kq/approval')" is-link>
+                    <img slot="icon" src="../../assets/img/11_icon.png" width="24" height="24">
+                 </mt-cell>
+                 <mt-cell title="异常处理" @click.native="goRouter('/kq/exception')" is-link>
+                    <img slot="icon" src="../../assets/img/5_icon.png" width="24" height="24">
+                 </mt-cell>
               </div>
            </div>
            <div class="menu-nav">
               <div class="title"><i class="leave-icon"></i>发起申请</div>
               <div class="menu-list clearfix">
-                 <div class="item">
-                    <router-link to="/kq/leave">
-                    <mt-badge size="small">3</mt-badge>
-                      <img src="../../assets/img/3_icon.png"><span>请假</span>
-                    </router-link>
-                 </div>
-                 <div class="item">
-                    <router-link to="/kq/yearLeave">
-                      <img src="../../assets/img/4_icon.png"><span>年休假</span>
-                    </router-link>
-                 </div>
-                 <div class="item">
-                    <router-link to="/kq/leaveQuery">
-                      <img src="../../assets/img/12_icon.png"><span>申请记录</span>
-                    </router-link>
-                 </div>
+                 <mt-cell title="请假" @click.native="goRouter('/kq/leave')" is-link>
+                    <img slot="icon" src="../../assets/img/3_icon.png" width="24" height="24">
+                 </mt-cell>
+                 <mt-cell title="年休假" @click.native="goRouter('/kq/yearLeave')" is-link>
+                    <img slot="icon" src="../../assets/img/4_icon.png" width="24" height="24">
+                 </mt-cell>
+                 <mt-cell title="申请记录" @click.native="goRouter('/kq/leaveQuery')" is-link>
+                    <img slot="icon" src="../../assets/img/12_icon.png" width="24" height="24">
+                 </mt-cell>
               </div>
            </div>
            <div class="menu-nav">
               <div class="title"><i class="query-icon"></i>相关查询</div>
               <div class="menu-list clearfix">
-                 <div class="item">
-                    <router-link to="/kq/cardRecord">
-                      <img src="../../assets/img/13_icon.png"><span>原始刷卡记录</span>
-                    </router-link>
-                 </div>
-                 <div class="item">
-                    <router-link to="/kq/calendar">
-                      <img src="../../assets/img/6_icon.png"><span>考勤日历</span>
-                    </router-link>
-                 </div>
+                 <mt-cell title="原始刷卡记录" @click.native="goRouter('/kq/cardRecord')" is-link>
+                    <img slot="icon" src="../../assets/img/13_icon.png" width="24" height="24">
+                 </mt-cell>
+                 <mt-cell title="考勤日历" @click.native="goRouter('/kq/calendar')" is-link>
+                    <img slot="icon" src="../../assets/img/6_icon.png" width="24" height="24">
+                 </mt-cell>
               </div>
            </div>
        </div>
@@ -82,7 +66,11 @@ export default {
             ]
         }
     },
-    methods:{}
+    methods:{
+        goRouter(url){
+            this.$router.push(url);
+        }
+    }
 }
 </script>
 <style lang="css" scoped>
@@ -94,5 +82,13 @@ export default {
     }
     .query-icon{
         background-image: url("../../assets/img/query-icon.png");
+    }
+    .menu-tree .menu-nav .title {
+        line-height: 0.7rem;
+        color: #999999;
+    }
+    .menu-list {
+        padding: 0 0 0 0.4rem;
+        overflow: hidden;
     }
 </style>
