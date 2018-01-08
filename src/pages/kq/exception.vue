06f19e6e69956144dacddb79957665bf5713bb4e
UEwby <1159018192@qq.com>
2018-01-08 09:15:56 +0800

    考勤异常+请假申请

--- src/pages/kq/exception.vue
+++ src/pages/kq/exception.vue
@@ -6,9 +6,17 @@
             <div class="info">
                 <!--<div class="fl text-over clearfix">部门：<span>***部</span></div>
                 <div class="fr text-over">请假人姓名：<span>***部</span></div>-->
-                <div class="box1">异常14次（未填单14次）<i class="sanjiao"></i></div>
+                <div class="box1">异常14次（未填单14次）</div>
                 <div class="line">
-
+                    <div class="juxing">
+                        <div class="point-grey point-lb"></div>
+                        <div class="point-grey point-rb"></div>
+                    </div>
+                    <div class="line-c">
+                        <div class="sanjiao"></div>
+                        <div class="line-c-line"></div>
+                        <div class="point-grey point-cb"></div>
+                    </div>
                 </div>
                 <div class="col">
                     <div class="color-grey">迟到2次</div>
@@ -69,7 +77,7 @@
         //height: 0.8rem;
         //line-height: 0.8rem;
         //margin: 0 auto;
-        padding: .25rem 0;
+        padding: .4rem 0 .5rem;
     }
     /*.info div{
         width: 50%;
@@ -89,23 +97,77 @@
         min-height: calc(100% - 1rem);
     }
     .box1{
-        //width: 51%;
         height: .8rem;
         background-color: #FF6054;
         line-height: .8rem;
         font-size: .35rem;
         color: #ffffff;
-        margin: 0 11%;
-        //padding: 0 11%;
+        margin: 0 15%;
         text-align:center;
-        border-radius:10px;
-    }
-    .sanjiao{
-        margin: 0 auto;
-
+        border-radius:5px;
     }
     .line{
-        height: 1.5rem;
+        //height: 1.5rem;
+        padding-top: .5rem;
+        position: relative;
+
+    }
+    .line .juxing{
+        margin: 0 12%;
+        border: 1px solid #ddd;
+        border-bottom: none;
+        height: .7rem;
+        line-height: .7rem;
+        position: relative;
+        padding: 0 3px;
+    }
+    .point-grey{
+        display: block;
+        position: absolute;
+        bottom: 0;
+        width: 6px;
+        height: 6px;
+        border-radius:50%;
+        background-color: #aaa;
+        z-index: 9;
+    }
+    .point-lb{
+        left:0;
+        margin-left: -3px;
+    }
+    .point-cb{
+        left:50%;
+        margin-left: -2px;
+        z-index: 99;
+    }
+    .point-rb{
+        right:0;
+        margin-right:-3px;
+    }
+    .line-c{
+        height:100%;
+        position: absolute;
+        top:0;
+        left:50%;
+        width:20px;
+        margin-left: -10px;
+    }
+    .sanjiao{
+        width:0;
+        height:0;
+        border-width:10px;
+        border-style:solid;
+        border-color: #ff6054 #fff #fff #fff;
+        z-index: 99;
+    }
+    .line-c-line{
+        width:1px;
+        height:100%;
+        background-color: #ddd;
+        position: absolute;
+        top:0;
+        left:50%;
+        border-top:10px solid #ff6054;
     }
     .col{
         display: flex;
@@ -114,12 +176,13 @@
         justify-content: space-between;
     }
     .col div{
-        border-radius: 10px;
-        line-height: .8rem;
-        font-size: .35rem;
-        height: .8rem;
-        text-align:center;
-        padding:0 .2rem;
+        border-radius: 5px;
+        line-height: .7rem;
+        font-size: .3rem;
+        height: .7rem;
+        text-align: center;
+        padding: 0 .5rem;
+        z-index: 9;
     }
     .color-grey{
         background-color: #ccc;
