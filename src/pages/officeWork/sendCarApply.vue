dccaeeef5617e80c28e7f1d8944173a74f93a3d1
wangjie233 <wang674947294@qq.com>
2018-01-09 09:36:52 +0800

    ‘工作餐管理

--- src/pages/officeWork/sendCarApply.vue
+++ src/pages/officeWork/sendCarApply.vue
@@ -1,8 +1,164 @@
 <template>
-    <div>派车申请</div>
+    <div class="body-box">
+        <div class="approved-head">
+            <div class="approved-head-row">
+                <div class="approved-head-warp">申请部门：<span class="approved-head-info">xxxx部</span></div>
+                <div class="approved-head-warp">经办人：<span class="approved-head-info">张三</span></div>
+            </div>
+            <div class="approved-head-row">
+                <div class="approved-head-warp">申请时间：<span class="approved-head-info">2017-08-09</span></div>
+                <div class="approved-head-warp">单据状态：<span class="approved-head-info">未提交</span></div>
+            </div>
+        </div>
+        <div class="approved-body">
+            <div class="approved-body-row bd-bottom-1 sendCarType">
+                <div class="approved-body-warp">
+                    派车类型：
+                    <div class="selectWrap">
+                        <select id="sendCarType" class="" name="">
+                            <option value="">市区内派车</option>
+                            <option value="">正常长途派车</option>
+                            <option value="">特殊长途派车</option>
+                        </select>
+                        <div class="selectBtn"></div>
+                    </div>
+                    乘车人数：
+                    <span class="approved-body-info">
+                        <input type="number" id="rideNum" name="" value="3">人
+                    </span>
+                    <span class="cue">*必填</span>
+                </div>
+            </div>
+            <div class="approved-body-row bd-bottom-1">
+                <div class="approved-body-warp">用车事由：
+                    <span class="approved-body-info">
+                        <input type="text" name="" value="接公司领导">
+                    </span>
+                    <span class="cue">*必填</span>
+                </div>
+            </div>
+            <div class="approved-body-row bd-bottom-1">
+                <div class="approved-body-warp">联系人：
+                    <span class="approved-body-info">
+                        <input type="text" name="" value="张三">
+                    </span>
+                    <span class="cue">*必填</span>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 <script>
     export default {
         name: 'sendCarApply'
     }
 </script>
+<style lang="css" scoped>
+    /* 自定义select,input样式 */
+    input,select {
+        color: #999;
+        border: 0;
+        height: 0.6rem;
+        line-height: 0.6rem;
+        outline:none;
+        background-color: #fff;
+        box-sizing: border-box;
+        -webkit-appearance: none;
+        -moz-appearance: none;
+        appearance: none;
+    }
+    input {
+        border:0;
+        font-size: 16px;
+    }
+    .selectWrap {
+        position: relative;
+        line-height: normal;
+        display: inline-block;
+        border-radius: 5px;
+        border: 1px solid #3497f5;
+    }
+    .selectWrap select {
+        width: 100%;
+        padding: 0 0.6rem 0 0.2rem;
+        border-radius: 5px;
+    }
+    .selectBtn {
+        width: 0.4rem;
+        border-radius: 0 5px 5px 0;
+        position: absolute;
+        top: -1px;
+        bottom: -1px;
+        right: -1px;
+        overflow: hidden;
+        background-color: #3497f5;
+    }
+    .selectBtn::before,.selectBtn::after {
+        content: "";
+        width: 100%;
+        height: 50%;
+        display: inline-block;
+        background-repeat: no-repeat;
+        background-position: center;
+        background-size: 50%;
+        background-image: url('../../assets/img/select-icon.png');
+        position: absolute;
+    }
+    .selectBtn::before {
+        top: 0;
+        right: 0;
+        transform: rotate(180deg);
+    }
+    .selectBtn::after {
+        bottom: 0;
+        right: 0;
+    }
+    /* 自定义select样式 end*/
+    .sendCarType .selectWrap {
+        width: 2.2rem;
+    }
+    .sendCarType #rideNum {
+
+    }
+    .body-box {
+        font-size: 14px;
+    }
+    .approved-head {
+        padding: 0.3rem;
+        background-color: #fff;
+    }
+    .approved-head .approved-head-row {
+        color: #333;
+        width: 100%;
+        margin-bottom: 0.25rem;
+        overflow: hidden;
+    }
+    .approved-head .approved-head-row:last-child {
+        margin-bottom: 0;
+    }
+    .approved-head .approved-head-warp {
+        width: 50%;
+        float: left;
+    }
+    .approved-head .approved-head-info {
+        color: #999;
+    }
+
+    .approved-body {
+        margin-top: 0.25rem;
+        padding: 0 0.3rem;
+        background-color: #fff;
+    }
+    .approved-body .approved-body-row {
+        height: 1rem;
+        line-height: 1rem;
+        overflow: hidden;
+    }
+    .approved-body .bd-bottom-1:before {
+        border-bottom:0;
+    }
+    .approved-body .cue {
+        float: right;
+        color: #ff6054;
+    }
+</style>
--- src/pages/officeWork/workLunchApply.vue
+++ src/pages/officeWork/workLunchApply.vue
@@ -1,8 +1,183 @@
 <template>
-    <div>工作餐申请</div>
+    <!--异常处理-->
+    <div class="body-box">
+        <TimeTool @selectTime="selectTime"></TimeTool>
+        <div class="application-list-box">
+            <div class="application-type-box bd-bottom-1">
+                <div class="application-type nnsubmitted">未提交</div>
+                <div class="application-type submitted">已提交</div>
+                <div class="application-type approved">已审批</div>
+                <div class="application-type confirmed">已确认</div>
+                <div class="application-type retreated">已退回</div>
+            </div>
+            <ul class="application-list">
+                <li class="application-list-cell">
+
+                    <div class="icon"></div>
+                    <div class="application-list-info bd-bottom-1">
+                        <p class="application-title-date">
+                            <span class="application-title">张三的工作餐申请<span class="submitted">（已提交）</span></span>
+                            <span class="application-date">2017-08-08</span>
+                        </p>
+                        <p class="eat-type">就餐类别：自助餐</p>
+                    </div>
+                </li>
+                <li class="application-list-cell">
+                    <div class="icon"></div>
+                    <div class="application-list-info bd-bottom-1">
+                        <p class="application-title-date">
+                            <span class="application-title">张三的工作餐申请<span class="approved">（已审批）</span></span>
+                            <span class="application-date">2017-08-08</span>
+                        </p>
+                        <p class="eat-type">就餐类别：自助餐</p>
+                    </div>
+                </li>
+                <li class="application-list-cell">
+                    <div class="icon"></div>
+                    <div class="application-list-info bd-bottom-1">
+                        <p class="application-title-date">
+                            <span class="application-title">张三的工作餐申请<span class="retreated">（已退回）</span></span>
+                            <span class="application-date">2017-08-08</span>
+                        </p>
+                        <p class="eat-type">就餐类别：自助餐</p>
+                    </div>
+                </li>
+                <li class="application-list-cell">
+                    <div class="icon"></div>
+                    <div class="application-list-info bd-bottom-1">
+                        <p class="application-title-date">
+                            <span class="application-title">张三的工作餐申请<span class="nnsubmitted">（未提交）</span></span>
+                            <span class="application-date">2017-08-08</span>
+                        </p>
+                        <p class="eat-type">就餐类别：自助餐</p>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
 </template>
 <script>
+    import TimeTool from "@/components/query/timetool";
     export default {
-        name: 'workLunchApply'
+        name: 'workLunchApply',
+        components:{TimeTool},
+        data(){
+            return {
+            }
+        },
+        mounted(){
+
+        },
+        methods:{
+            selectTime(startTime,endTime){
+                console.log(startTime);
+                console.log(endTime);
+            }
+        }
     }
 </script>
+<style lang="css" scoped>
+    .mint-cell-wrapper {
+        padding: 0;
+    }
+    .application-list-box{
+        padding: 0 0.3rem;
+        background-color: #fff;
+    }
+    .application-type-box {
+        display: flex;
+        padding: 0.4rem 0;
+    }
+    .application-type-box .application-type {
+        flex:1;
+        color: #fff;
+        font-size: 14px;
+        padding: 0.2rem 0;
+        border-radius: 5px;
+        text-align: center;
+        margin-left: 0.3rem;
+        background-color: #aaa;
+    }
+    .application-type-box .nnsubmitted {
+        background-color: #ff8922;
+    }
+    .application-type-box .approved {
+        background-color: #11bf84;
+    }
+    .application-type-box .retreated {
+        background-color: #f3725f;
+    }
+    .application-type-box .application-type:first-child {
+        margin-left: 0;
+    }
+
+    .application-list-box .application-list{
+    }
+    .application-list .application-list-cell {
+        display: flex;
+        align-items: center;
+    }
+    .application-list .icon {
+        width: 0.7rem;
+        height: 0.7rem;
+        border-radius: 5px;
+        background-color: #bbb;
+        background-image: url('../../assets/img/workLunch-icon.png');
+        background-repeat: no-repeat;
+        background-size: 65%;
+        background-position: center;
+    }
+    .application-list .application-list-info {
+        flex: 1;
+        font-size: 14px;
+        padding: 0.35rem 0;
+        margin-left: 0.3rem;
+    }
+    .application-list .application-title-date {
+        margin-bottom: 0.3rem;
+    }
+    .application-list .application-title {
+        color: #000;
+        font-size: 16px;
+    }
+    .application-list .application-date {
+        color: #666;
+        float: right;
+        line-height: 21px;
+    }
+    .application-list .eat-type {
+        color: #999;
+    }
+    .application-list .application-title span {
+        color: #aaa;
+    }
+    .application-list .application-title .nnsubmitted {
+        color: #ff8922;
+    }
+    .application-list .application-title .approved {
+        color: #11bf84;
+    }
+    .application-list .application-title .retreated {
+        color: #f3725f;
+    }
+
+    .application-list .application-list-cell:last-child .bd-bottom-1:before {
+        border-bottom:0;
+    }
+
+    @media screen and (max-width: 320px) {
+        .application-type-box .application-type {
+            font-size: 12px
+        }
+        .application-list .application-list-info {
+            font-size: 12px;
+        }
+        .application-list .application-title {
+            color: #000;
+            font-size: 14px;
+        }
+        .application-list .application-date {
+            line-height: 19px;
+        }
+    }
+</style>
