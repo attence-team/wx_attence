abf769728f6e17989d481d1ec802870810af225c
wangjie233 <wang674947294@qq.com>
2018-01-12 18:37:17 +0800

    下拉刷新加载

--- src/pages/officeWork/workLunchQuery.vue
+++ src/pages/officeWork/workLunchQuery.vue
@@ -11,65 +11,68 @@
                 <div class="application-type retreated" @click="changeLd_appr('2')">已退回</div>
             </div>
         </div>
-        <div class="application-list-box" id="list-box" v-show="listBox">
-            <ul class="application-list">
-                <router-link v-for="item in listDataArr" :to="'workLunchApply?id=' + item.dinner_id ">
-                <li class="application-list-cell">
-                    <div class="icon"></div>
-                    <div class="application-list-info bd-bottom-1">
-                        <div class="application-list-info-wrap">
-                            <p class="application-title-date">
-                                <span class="application-title-wrap"><span class="application-title">{{item.dir_title}}</span><!--<span class="submitted">（已提交）</span>--></span>
-                                <span class="application-date">{{item.apply_time | date}}</span>
-                            </p>
-                            <p class="eat-type">就餐类别：{{item.stand_name}}</p>
+        <div class="mescroll-wrap" id="listBox" v-show="listBox">
+            <div class="application-list-box mescroll" id="mescroll">
+                <ul class="application-list">
+                    <router-link v-for="item in listDataArr" :to="'workLunchApply?id=' + item.dinner_id ">
+                    <li class="application-list-cell">
+                        <div class="icon"></div>
+                        <div class="application-list-info bd-bottom-1">
+                            <div class="application-list-info-wrap">
+                                <p class="application-title-date">
+                                    <span class="application-title-wrap"><span class="application-title">{{item.dir_title}}</span><!--<span class="submitted">（已提交）</span>--></span>
+                                    <span class="application-date">{{item.apply_time | formatDate}}</span>
+                                </p>
+                                <p class="eat-type">就餐类别：{{item.stand_name}}</p>
+                            </div>
                         </div>
-                    </div>
-                </li>
-                </router-link>
-                <!-- <router-link to="workLunchApply">
-                <li class="application-list-cell">
-                    <div class="icon"></div>
-                    <div class="application-list-info bd-bottom-1">
-                        <p class="application-title-date">
-                            <span class="application-title">张三的工作餐申请<span class="approved">（已审批）</span></span>
-                            <span class="application-date">2017-08-08</span>
-                        </p>
-                        <p class="eat-type">就餐类别：自助餐</p>
-                    </div>
-                </li>
-                </router-link>
-                    <router-link to="workLunchApply">
-                <li class="application-list-cell">
-                    <div class="icon"></div>
-                    <div class="application-list-info bd-bottom-1">
-                        <p class="application-title-date">
-                            <span class="application-title">张三的工作餐申请<span class="retreated">（已退回）</span></span>
-                            <span class="application-date">2017-08-08</span>
-                        </p>
-                        <p class="eat-type">就餐类别：自助餐</p>
-                    </div>
-                </li>
-                </router-link>
-                    <router-link to="workLunchApply">
-                <li class="application-list-cell">
-                    <div class="icon"></div>
-                    <div class="application-list-info bd-bottom-1">
-                        <p class="application-title-date">
-                            <span class="application-title">张三的工作餐申请<span class="nnsubmitted">（未提交）</span></span>
-                            <span class="application-date">2017-08-08</span>
-                        </p>
-                        <p class="eat-type">就餐类别：自助餐</p>
-                    </div>
-                </li>
-                </router-link> -->
-            </ul>
+                    </li>
+                    </router-link>
+                    <!-- <router-link to="workLunchApply">
+                    <li class="application-list-cell">
+                        <div class="icon"></div>
+                        <div class="application-list-info bd-bottom-1">
+                            <p class="application-title-date">
+                                <span class="application-title">张三的工作餐申请<span class="approved">（已审批）</span></span>
+                                <span class="application-date">2017-08-08</span>
+                            </p>
+                            <p class="eat-type">就餐类别：自助餐</p>
+                        </div>
+                    </li>
+                    </router-link>
+                        <router-link to="workLunchApply">
+                    <li class="application-list-cell">
+                        <div class="icon"></div>
+                        <div class="application-list-info bd-bottom-1">
+                            <p class="application-title-date">
+                                <span class="application-title">张三的工作餐申请<span class="retreated">（已退回）</span></span>
+                                <span class="application-date">2017-08-08</span>
+                            </p>
+                            <p class="eat-type">就餐类别：自助餐</p>
+                        </div>
+                    </li>
+                    </router-link>
+                        <router-link to="workLunchApply">
+                    <li class="application-list-cell">
+                        <div class="icon"></div>
+                        <div class="application-list-info bd-bottom-1">
+                            <p class="application-title-date">
+                                <span class="application-title">张三的工作餐申请<span class="nnsubmitted">（未提交）</span></span>
+                                <span class="application-date">2017-08-08</span>
+                            </p>
+                            <p class="eat-type">就餐类别：自助餐</p>
+                        </div>
+                    </li>
+                    </router-link> -->
+                </ul>
+            </div>
         </div>
     </div>
 </template>
 <script>
     import { WlHttp } from '@/api/workLunchHttp';
     import TimeTool from "@/components/query/timetool";
+    window.Mescroll = require('../../assets/js/mescroll.min.js');
     export default {
         name: 'workLunchQuery',
         components:{TimeTool},
@@ -87,17 +90,18 @@
             }
         },
         mounted(){
+            this.creatMescroll();
         },
         activated(){
         },
         filters: {
-          date: function (value) {
-             return value.Format2String('yyyy-MM-dd');
+          formatDate: function (value) {
+              if(value.length!=8) {return value;}
+             return value.slice(0,4) + '-' + value.slice(4,6) + '-' + value.slice(6,8);
           }
         },
         methods:{
-            getWorkLunchList(load){
-                if(!load) { this.listDataArr = []; }
+            getWorkLunchList(){
                 let params = {
                     staff_num:this.staff_num,
                     sdate:this.sdate,
@@ -108,55 +112,84 @@
                     pageLength:this.pageLength
                 };
                 WlHttp.getWorkLunchList(params).then((res)=>{
-                    console.log(res);
+                    //console.log(res);
                     if (res.code=='1') {
                         let pageData = res.data.pageData;
-                        let _this = this;
+                        let _this = this,hasNext = true;
+
+                        if( this.currPage == 1) { _this.listDataArr = []; }
+                        if (pageData.length < this.pageLength) { hasNext = false; }
+
                         pageData.map(function(e) {
                             _this.listDataArr.push(e);
                         })
-                        //alert(JSON.stringify(res))    
-                        this.setListTop();
+                        this.mescroll.endSuccess(20,hasNext);
+                    }else{
+                        this.mescroll.endErr();
                     }
                 });
             },
             changeLd_appr(index){
                 this.ld_appr = index;
-                this.getWorkLunchList();
+                this.mescroll.resetUpScroll();
             },
             changeDir_appr(index){
                 this.dir_appr = index;
-                this.getWorkLunchList();
+                this.mescroll.resetUpScroll();
             },
             selectTime(startTime,endTime){
                 this.sdate = startTime.Format2String('yyyyMMdd');
                 this.edate = endTime.Format2String('yyyyMMdd');
+                this.mescroll.resetUpScroll();
+            },
+            creatMescroll(){
+                 let _this = this;
+
+                 setTimeout(function() {
+                    let header = document.getElementById('header'),
+                        listBox = document.getElementById('listBox');
+                        listBox.style.top = header.clientHeight + 'px';
+                 },1)
+
+                 _this.mescroll = new Mescroll("mescroll", {
+    	       		down: {
+                        auto:false,
+                        offset:50,
+    					callback: _this.downCallback //下拉刷新的回调
+    				},
+    				up: {
+                        auto:false,
+                        htmlNodata:'<p class="upwarp-nodata">-- 暂无数据 --</p>',
+    					callback: _this.upCallback , //上拉加载的回调
+    					isBounce: true
+    				}
+    			});
+
+                this.listBox = true; //显示列表容器
+            },
+            downCallback(){
+                this.currPage = 1;
+                this.mescroll.setPageNum(this.currPage); //重置mescroll当前页数
                 this.getWorkLunchList();
             },
-            setListTop(){
-                 let header = document.getElementById('header'),
-                     listBox = document.getElementById('list-box');
-                     listBox.style.top = header.clientHeight + 'px';
-
-                     this.listBox = true;
+            upCallback(page){ //上拉加载的回调 page = {num:1, size:10}; num:当前页 默认从1开始, size:每页数据条数,默认10
+                //console.log(page.num);
+                this.currPage = page.num;
+                this.getWorkLunchList();
             }
         }
     }
 </script>
 <style lang="css" scoped>
+@import "../../assets/css/mescroll.min.css";
 .body-box {
     position: relative;
-    overflow: hidden;
 }
-#list-box {
+.mescroll-wrap {
     position: absolute;
-    overflow: hidden;
     bottom: 0;
     left: 0;
     right: 0;
-}
-#list-box ul {
-    height: 100%;
-    overflow: auto;
+    overflow: hidden;
 }
 </style>
