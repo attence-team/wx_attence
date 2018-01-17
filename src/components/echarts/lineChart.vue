0046604a250a50ec1483dd198a2140accd021255
yyg <yygwork@126.com>
2018-01-17 13:20:39 +0800

    构建工具配置

--- src/components/echarts/lineChart.vue
+++ src/components/echarts/lineChart.vue
@@ -1,135 +1,19 @@
 <template>
-    <div ref="chart" style="height:100%;width:100%;position: relative;">
+    <div style="height:100%;width:100%;position: relative;">
+      <iframe src="./chart/line.html" style="height:100%;width:100%;" frameborder="no" border="0" scrolling="no" allowtransparency="yes"></iframe>
     </div>
 </template>
 <script>
-    import echarts from 'echarts';
     export default {
         name: 'lineChart',
         activated(){
             this.$nextTick(()=>{
-                this.initChart(echarts.init(this.$refs.chart));
+                this.initChart();
             })
         },
         methods: {
-            initChart(myChart) {
-                let option = {
-                    tooltip : {
-                        trigger: 'axis'
-                    },
-                    legend: {
-                        top:'0',
-                        left:'4.5%',
-                        icon:'circle',
-                        data:[
-                            {name:'可挑拨库存',textStyle:{color:"#74d5fd"}},
-                            {name:'产量',textStyle:{color:"#58ffc6"}},
-                            {name:'调拨量',textStyle:{color:"#dddddd"}}
-                        ]
-                    },
-                    grid: {
-                        top:'12%',
-                        left: '0',
-                        right: '8%',
-                        bottom: '5%',
-                        containLabel: true
-                    },
-                    xAxis : [
-                        {
-                            type : 'category',
-                            boundaryGap: false,
-                            axisTick:{show:false},
-                            axisLine: {
-                                lineStyle: {
-                                    color: '#7380ad'
-                                }
-                            },
-                            axisLabel:{
-                                textStyle:{
-                                    color:"#fff"
-                                }
-                            },
-                            splitLine: {
-                                show: true,
-                                lineStyle: {
-                                    color: 'rgba(101,148,129,0.8)',
-                                    type:'dotted',
-                                    width: 1,
-                                }
-                            },
-                            data : ['周二\n01/02','周三\n01/03','周四\n01/04','周五\n01/05','周六\n01/06','昨天\n01/07','今天\n01/07']
-                        }
-                    ],
-                    yAxis : [
-                        {
-//                            type : 'value',
-                            name:'单位：箱',
-                            nameTextStyle:{
-                                color:'#ddd'
-                            },
-                            axisLine: {show:false},
-                            axisTick:{show:false},
-                            axisLabel:{
-                                show:false
-                            },
-                            splitLine: {
-                                show: false
-                            },
-                            splitArea: {
-                                show: false
-                            }
-                        }
-                    ],
-                    color:["#74d5fd","#58ffc6","#dddddd"],
-                    series : [
-                        {
-                            name:'可挑拨库存',
-                            type:'line',
-                            barWidth:15,
-                            symbolSize:8,
-                            itemStyle: {
-                                normal: {
-                                    label: {
-                                        show: true,
-                                        position: "top"
-                                    }
-                                }
-                            },
-                            data:[2000, 1000, 2000, 6000, 36067, 10850,20866.1]
-                        },
-                        {
-                            name:'产量',
-                            type:'line',
-                            barWidth:15,
-                            symbolSize:8,
-                            itemStyle: {
-                                normal: {
-                                    label: {
-                                        show: true,
-                                        position: "top"
-                                    }
-                                }
-                            },
-                            data:[1002, 2000, 5000, 3000, 500, 2700,5850]
-                        },
-                        {
-                            name:'调拨量',
-                            type:'line',
-                            barWidth:15,
-                            symbolSize:8,
-                            itemStyle: {
-                                normal: {
-                                    label: {
-                                        show: true,
-                                        position: "top"
-                                    }
-                                }
-                            },
-                            data:[1002, 2000, 5000, 3400, 5700, 4700,8850]
-                        }
-                    ]
-                };
-                myChart.setOption(option);
+            initChart() {
+
             }
         }
     }
--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from './App';
 import router from './router';
 /* common存放公用变量，函数和初始化操作 */
 import '@/assets/js/common';
-import './assets/js/filter';
 import MintUI from 'mint-ui';
 import 'mint-ui/lib/style.css';
 import '@/assets/css/index.css';
